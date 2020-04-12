c883c5c30e0594ad5da91ea6798b3c10270dfb63
Nguyễn Hồng Quân <ng.hong.quan@gmail.com>
2020-04-12 12:15:00 +0700

    Define signal handlers in Glade file

--- cobang/app.py
+++ cobang/app.py
@@ -1,8 +1,12 @@
 import gi
+from logbook import Logger
 gi.require_version('Gtk', '3.0')
-from gi.repository import Gtk, Gio  # noqa
+from gi.repository import Gtk, Gio, Gdk  # noqa
 
 from .resources import get_ui_filepath
+
+
+logger = Logger(__name__)
 
 
 class CoBangApplication(Gtk.Application):
@@ -17,20 +21,34 @@ class CoBangApplication(Gtk.Application):
     def do_startup(self):
         Gtk.Application.do_startup(self)
         action = Gio.SimpleAction.new("quit", None)
-        action.connect("activate", self.on_quit)
+        action.connect("activate", self.quit_from_action)
         self.add_action(action)
 
     def build_main_window(self):
         source = get_ui_filepath('main.glade')
         builder: Gtk.Builder = Gtk.Builder.new_from_file(str(source))
+        handlers = self.signal_handlers_for_glade()
+        builder.connect_signals(handlers)
         window: Gtk.Window = builder.get_object('main-window')
         builder.get_object('main-grid')
         window.set_application(self)
         self.set_accels_for_action('app.quit', ('<Ctrl>Q',))
-        btn_quit = builder.get_object('btn-quit')
-        btn_quit.connect('clicked', self.on_quit, self)
         builder.get_object('stack-img-source')
         return window
+
+    def signal_handlers_for_glade(self):
+        return {
+            'on_area_webcam_realize': self.pass_window_to_gstreamer,
+            'on_btn_quit_clicked': self.quit_from_widget
+        }
+
+    def pass_window_to_gstreamer(self, area_webcam: Gtk.DrawingArea):
+        window: Gdk.Window = area_webcam.get_window()
+        is_native = window.ensure_native()
+        if not is_native:
+            logger.error('DrawingArea {} is not a native window', area_webcam.get_name())
+            return
+        print(window)
 
     def do_activate(self):
         if not self.window:
@@ -41,5 +59,8 @@ class CoBangApplication(Gtk.Application):
         self.activate()
         return 0
 
-    def on_quit(self, action, param):
+    def quit_from_widget(self, widget: Gtk.Widget):
+        self.quit()
+
+    def quit_from_action(self, action, param):
         self.quit()
