fc4c8fd717337b8cb20ede14cd02ca743cbff202
Nguyễn Hồng Quân <ng.hong.quan@gmail.com>
2020-05-25 15:35:15 +0700

    Move data folder out again.
    We have no hope to tell Python tool to install data files to places
    which are compatible with desktop app way.

--- cobang/resources.py
+++ cobang/resources.py
@@ -7,7 +7,7 @@ from pathlib import Path
 
 
 def get_ui_folder() -> Path:
-    top_app_dir = Path(__file__).parent
+    top_app_dir = Path(__file__).parent.parent
     data_folder = top_app_dir / 'data'
     if data_folder.exists():
         return data_folder
